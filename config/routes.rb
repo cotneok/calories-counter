3a0214ebc159ef889aca01fc60a0e8b145d6e0d0
Tsotne Okrostsvaridze <cotne.ok@gmail.com>
2020-02-27 01:37:22 +0400

    Add users controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :meals
   devise_for :users
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#home'
 end
