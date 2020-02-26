753fde31758e012fc59caad1abeebefd82eeab75
Tsotne Okrostsvaridze <cotne.ok@gmail.com>
2020-02-27 02:18:27 +0400

    Add users show page with meals listing for the user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,15 @@
 class UsersController < ApplicationController
+	def index 
+		@user = User.new
+	end 
+
+	def show 
+		@user = User.find(params[:id])
+	end 
+
+	private 
+
+	def user_params
+    	params.require(:user).permit(:email, :password)
+  	end
 end
