efcb3701e50f4a2d31808960894cd66febe6e0ca
Tsotne Okrostsvaridze <cotne.ok@gmail.com>
2020-02-27 03:40:34 +0400

    Add some styling to navbar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 	def index 
-		@user = User.new
+		@users = User.all
 	end 
 
 	def show 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ApplicationRecord
-	has_many :meals
+  has_many :meals
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :username, presence: true,
+            uniqueness: { case_sensitive: false },
+            length: { minimum: 3, maximum: 25 }
 end
