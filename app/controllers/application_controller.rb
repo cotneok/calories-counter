9bd53e072f0d4d9e74f31ecbb4c04fd7245a10be
Tsotne Okrostsvaridze <cotne.ok@gmail.com>
2020-02-27 04:07:01 +0400

    Added insallation requirements for petergate

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,19 @@
 class ApplicationController < ActionController::Base
 	before_action :authenticate_user!
-	helper_method :current_user, :logged_in?
+	#helper_method :current_user, :logged_in?
 
 	# def current_user
  #    @current_user ||= User.find(session[:user_id]) if session[:user_id]
  #  end
 
-  def logged_in?
-    !!current_user
-  end
+  # def logged_in?
+  #   !!current_user
+  # end
 
-  def require_user
-    if !logged_in?
-      flash[:danger] = "You must be logged in to perform that action"
-      redirect_to root_path
-    end
-  end
+  # def require_user
+  #   if !logged_in?
+  #     flash[:danger] = "You must be logged in to perform that action"
+  #     redirect_to root_path
+  #   end
+  # end
 end
