3bac1d70420e31c3528ae5c04a40b97a18017244
Tsotne Okrostsvaridze <cotne.ok@gmail.com>
2020-02-27 21:55:51 +0400

    Add crud for user_manager

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,27 @@ class UsersController < ApplicationController
 		@users = User.all
 	end 
 
+	def new
+		@user = User.new
+	end 
+
 	def show 
 		@user = User.find(params[:id])
 	end 
+
+	def edit 
+		@user = User.find(params[:id])
+	end 
+
+	def update
+		@user = User.find(params[:id])
+	    if @user.update(user_params)
+	      flash[:success] = "Your account was updated successfully"
+	      redirect_to root_path
+	    else
+	      render 'edit'
+	    end
+  	end
 
 	private 
 
