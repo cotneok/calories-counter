70aff4307e75045d451564de2528908ab39af0ee
Tsotne Okrostsvaridze <cotne.ok@gmail.com>
2020-02-29 03:33:21 +0400

    Add 3 roles in seed.rb for production

--- README.md
+++ README.md
@@ -2,4 +2,4 @@ Calories Counter APP
 
 Rails: 5.2.4.1
 Ruby: 2.5.5
-Link production version: 
+Link production version: https://calories-counter-tsotne.herokuapp.com/
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(username: "admin", email: "admin@example.com", roles: "site_admin" ,password: "password")
+User.create!(username: "moderator", email: "moder@example.com", roles: "user_manager" ,password: "password")
+User.create!(username: "user", email: "user@example.com", roles: "user" ,password: "password")
