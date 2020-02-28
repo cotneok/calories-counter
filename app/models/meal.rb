a98f16234476c2d18469386c83d500cc5bcbbde0
Tsotne Okrostsvaridze <cotne.ok@gmail.com>
2020-02-28 04:46:53 +0400

    nothing

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,4 +1,17 @@
 class Meal < ApplicationRecord
+	attr_accessor :time, :search
 	belongs_to :user
 	validates :user_id, presence: true
+
+  def self.time_filter(meals,period1,period2)
+    meals.reject { |meal| meal.date < period1 }.reject{|meal| meal.date >period2}
+  end
+
+  def self.admin_time_filter(meals,period1,period2)
+    meals.reject { |meal| meal.date < period1 }.reject{|meal| meal.date >period2}
+  end
+
+  def self.search(meals,period1,period2)
+    meals.reject { |meal| meal.date < period1}.reject { |meal| meal.date > period2}.reject { |meal| meal.date.hour > period2.hour}.reject { |meal| meal.date.hour < period1.hour}
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :meals
   devise_for :users
   resources :users
+  get 'search', to: 'meals#search'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#home'
 end
