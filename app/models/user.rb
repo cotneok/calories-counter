cef9f11317c69170e6ee6f034b6ac45e140ae4b7
Tsotne Okrostsvaridze <cotne.ok@gmail.com>
2020-02-29 02:38:25 +0400

    Small changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,4 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :username, presence: true,
-            uniqueness: { case_sensitive: false },
-            length: { minimum: 3, maximum: 25 }
 end
